79cd220743e6bba0ffc45b07a861fe9fdc72cc60
nensi00 <ahorbeshko@gmail.com>
2025-03-22 12:56:28 +0100

    commit

--- Java-Script/lessons/02_lesson/022/js/main.js
+++ Java-Script/lessons/02_lesson/022/js/main.js
@@ -5,15 +5,15 @@ let out = document.querySelector ('.out')
 btn.onclick = () => {
 
     if (+age.value >= 18 && +age.value < 25) {
-            out.innerHTML = 'Придатний(-а) на строкову службу';
+            out.innerHTML = 'На ДОНБАС! Придатний(-а) на строкову службу';
         }
         else if ( +age.value >= 25 && +age.value < 60) {
-                out.innerHTML = 'Придатний(-а) на Контрактну службу';
+                out.innerHTML = 'На ДОНБАС! Придатний(-а) на Контрактну службу';
             }
         else if ( +age.value < 14) {
-            out.innerHTML = 'Число повинно бути не менше 14';
+            out.innerHTML = 'Ви ще маленькі! Число повинно бути не менше 14';
         }
         else {
-            out.innerHTML = 'Не придатний(-а)';
+            out.innerHTML = 'Оце тобі повезло. Не придатний(-а)';
         }
 }
